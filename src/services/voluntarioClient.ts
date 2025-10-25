7f68abfb5c5c2774e37eb0bb38fd41262c0b7659
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-25 14:02:56 -0300

    chore(src/services): atualiza interface de voluntário
    Remove a opcionalidade de alguns campos da interface `Voluntário`
    por questões de `linting`.

--- src/services/voluntarioClient.ts
+++ src/services/voluntarioClient.ts
@@ -1,16 +1,16 @@
 export interface Voluntario {
   id?: number;
   nome: string;
-  apelido?: string;
-  idade?: number;
-  tempo_pratica?: string;
+  apelido: string;
+  idade: number;
+  tempo_pratica: string;
   documento_id?: string;
-  peso?: number;
-  altura?: number;
-  graduacao?: string;
-  genero?: string;
-  endereco?: string;
-  contato?: string;
+  peso: number;
+  altura: number;
+  graduacao: string;
+  genero: string;
+  endereco: string;
+  contato: string;
   created_at?: string;
 }
 
