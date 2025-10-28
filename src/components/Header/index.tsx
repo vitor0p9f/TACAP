2e16952cfc0f705b591e3d4cb0933711843f6c45
Athylas <182241660+athyy1@users.noreply.github.com>
2025-10-28 00:13:38 -0300

    refactor(header): Transforma o Header em um componente controlado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { MagnifyingGlassIcon, FunnelIcon, UploadSimpleIcon } from '@phosphor-icons/react';
 import * as S from './styles';
 
-export function Header() {
+interface HeaderProps {
+  searchTerm: string;
+  setSearchTerm: (term: string) => void;
+}
+
+export function Header({ searchTerm, setSearchTerm }: HeaderProps) {
   return (
     <S.Container>
       <S.SearchBarWrapper>
         <MagnifyingGlassIcon size={20} />
-        <input type="search" placeholder="Busca..." />
+        <input
+          type="search"
+          placeholder="Busca..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </S.SearchBarWrapper>
       <S.HeaderActions>
         <button title="Filtrar">
