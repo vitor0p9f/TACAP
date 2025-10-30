10d6527a23ae3f19e6956091aba7a5fecd6d4a2e
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-30 16:03:57 -0300

    feat(src/components): atualiza o componente de Input
    Atualiza a função `onKeyDownHandler` para validar a inserção
    correta de números de ponto flutuante.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -28,17 +28,41 @@ const Input: React.FC<InputProps> = ({label, mask, className, value, ...rest}) =
     }
 
     const onKeyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const hasStep = rest.step !== undefined;
+    
         const allowedKeys = [
-            "Backspace",
-            "Delete",
-            "ArrowLeft",
-            "ArrowRight",
-            "Tab"
+          "Backspace",
+          "Delete",
+          "ArrowLeft",
+          "ArrowRight",
+          "Tab",
         ];
       
-        if (!/[0-9]/.test(event.key) && !allowedKeys.includes(event.key)) {
-            event.preventDefault();
+        // 1. Permite números (0-9) e teclas de controle
+        if (/[0-9]/.test(event.key) || allowedKeys.includes(event.key)) {
+            return
         }
+        
+        // 2. Lógica do ponto
+        if (hasStep && event.key === ".") {
+            const currentValue = event.currentTarget.value
+            
+            // Bloqueia se já houver um ponto
+            if (/,/.test(currentValue)) {
+                event.preventDefault()
+                return
+            }
+            
+            // Bloqueia se o ponto for a primeira entrada (".9")
+            if (currentValue.length === 0) {
+                event.preventDefault()
+                return
+            }
+            return
+        }
+        
+        // 3. Bloqueia qualquer outra coisa (letras, símbolos, etc.)
+        event.preventDefault()
     }
 
     return (
